67249ad3096efc59bf9d5db70d48a7bcd15e8793
204Constie <kat.bana204@gmail.com>
2018-01-18 22:58:18 +0100

    new spk commit

--- Experiment1.cpp
+++ Experiment1.cpp
@@ -72,13 +72,13 @@ long Experiment::singleExperimentResult() {
 	// int seed = (unsigned)(random() * (omp_get_thread_num()+2));
 
 	struct plantSeed{
-		plantSeed(){
+		plantSeed(bool b){
 			int seed = (unsigned)(random() * (omp_get_thread_num()+2));
 			srand48_r(seed, &drand_Buffor);
 		}
 
 	}
-	plantSeed ps;
+	plantSeed ps(true);
 
 
   cout << "7 "  << endl;
